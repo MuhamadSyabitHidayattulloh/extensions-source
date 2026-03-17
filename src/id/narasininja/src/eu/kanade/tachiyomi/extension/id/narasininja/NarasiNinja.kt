cde5455cdb41d1d711b55d43eac6e5eed316d65b
MuhamadSyabitHidayattulloh <201265988+MuhamadSyabitHidayattulloh@users.noreply.github.com>
2026-03-17 13:07:10 +0000

    Add Indonesian source: Narasi Ninja
    - Implement Narasi Ninja source by overriding MangaThemesia theme.
    - Minimal implementation with correct baseUrl and mangaUrlDirectory.
    - Set isNsfw to false.
    - Include necessary build configuration and icons.

---
+++ src/id/narasininja/src/eu/kanade/tachiyomi/extension/id/narasininja/NarasiNinja.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.id.narasininja
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class NarasiNinja :
+    MangaThemesia(
+        "Narasi Ninja",
+        "https://narasininja.net",
+        "id",
+        "/komik",
+    )
